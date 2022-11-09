875bf61c66a680c139e6f8d34c2e5642e3372a18
Herdez <mherdez@users.noreply.github.com>
2022-11-09 14:51:45 -0600

    pokemon api

--- app.js
+++ app.js
@@ -15,20 +15,36 @@ const container = document.querySelector('.container');
 //   console.log(results[0].name.first)
 // }))
 
-const getUser = async () => {
-  const resp = await fetch('https://randomuser.me/api/');
+// const getUser = async () => {
+//   const resp = await fetch('https://randomuser.me/api/');
+//   const data = await resp.json();
+//   const usuario = data.results[0];
+
+//   console.log(usuario)
+
+//   container.innerHTML = `
+//     <img src="${ usuario.picture.large }" width="200">
+//     <h1>${usuario.name.first} ${usuario.name.last}</h1>
+
+//   `
+// }
+
+const getPokemon = async ( id ) => {
+  const resp = await fetch('https://pokeapi.co/api/v2/pokemon/' + id);
   const data = await resp.json();
-  const usuario = data.results[0];
 
-  console.log(usuario)
 
-  container.innerHTML = `
-    <img src="${ usuario.picture.large }" width="200">
-    <h1>${usuario.name.first} ${usuario.name.last}</h1>
+  console.log(data)
+
+  container.innerHTML += `
+    <img src="${ data.sprites.other.dream_world.front_default }" width="200">
+    <h1>${data.name}</h1>
 
   `
-
-
 }
 
-getUser()
+getPokemon(100)
+getPokemon(10)
+getPokemon(30)
+getPokemon(50)
+getPokemon(33)
